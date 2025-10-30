db861e647b7c90d9c957b0b89a680ca10d8c1050
mangala namal <mangalanamal@gmail.com>
2025-10-30 09:31:21 +0530

    first Commit

---
+++ MainForm.cs
@@ -0,0 +1,10 @@
+namespace LANMIC_ComBank_Interface
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
